81aa6091cb1e5cb56090b908c3edb0264b78a4fe
Rahul Gupta <104451837+rs4285@users.noreply.github.com>
2026-08-02 16:31:39 +0530

    Add: concatenation-of-array - submission-8

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+      ArrayList<Integer> list = new ArrayList<>();
+
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+
+    }
+}
